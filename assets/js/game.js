e38d038a83f20e928c1bc9b0373d51ba2dd588f7
Kevin Smith <kevin@kvnsmith.com>
2018-02-09 17:41:50 -0700

    game clicks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,59 +1,68 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// Global Variables
-var crystalVal1, crystalVal2, crystalVal3, crystalVal4;
-var wins = 0;
-var losses = 0;
-var randComputer;
-// counts user score
-var usrScore;
+	var crystalVal1, crystalVal2, crystalVal3, crystalVal4;
+	var wins = 0;
+	var losses = 0;
+	var randComputer;
+	// counts user score
+	var usrScore;
 
-//Functions
-startGame();
-function startGame () {
-// random number between 25 & 80
+	//Functions
+	startGame();
+	function startGame() {
+		// random number between 25 & 80
 
-randComputer = 25 + Math.floor(Math.random() * 55);
-crystalVal1 = 1 + Math.floor(Math.random() * 10);
-crystalVal2 = 1 + Math.floor(Math.random() * 10);;
-crystalVal3 = 1 + Math.floor(Math.random() * 10);
-crystalVal4 = 1 + Math.floor(Math.random() * 10);
-usrScore = 0;
-$('#winsCounter').html("Wins: " + wins);
-$('#lossesCounter').html("Losses: " + losses);
-$('#randomNumber').html(randComputer);
-$('userScore').html("Score: " + usrScore);
-console.log("computer value " + randComputer);
-console.log("crystal1 val " + crystalVal1);
-console.log("crystal2 val " + crystalVal2);
-console.log("crystal3 val " + crystalVal3);
-console.log("crystal4 val " + crystalVal4);
-console.log("user score " + usrScore);
-}
-//end startGame
-
-function winOrLose() {
-	// check if user has lost
-	if (wins > randComputer) {
-		losses++;
-		console.log("User Lost");
-		startGame();
+		randComputer = 25 + Math.floor(Math.random() * 55);
+		crystalVal1 = 1 + Math.floor(Math.random() * 10);
+		crystalVal2 = 1 + Math.floor(Math.random() * 10);;
+		crystalVal3 = 1 + Math.floor(Math.random() * 10);
+		crystalVal4 = 1 + Math.floor(Math.random() * 10);
+		usrScore = 0;
+		$('#winsCounter').html("Wins: " + wins);
+		$('#lossesCounter').html("Losses: " + losses);
+		$('#randomNumber').html(randComputer);
+		$('userScore').html("Score: " + usrScore);
+		console.log("computer value " + randComputer);
+		console.log("crystal1 val " + crystalVal1);
+		console.log("crystal2 val " + crystalVal2);
+		console.log("crystal3 val " + crystalVal3);
+		console.log("crystal4 val " + crystalVal4);
+		console.log("user score " + usrScore);
 	}
+	//end startGame
 
-	// check if user has won
-	if (usrScore == randComputer) {
-		wins++;
-		console.log("User Won");
-		startGame();
+	function winOrLose() {
+		// check if user has lost
+		if (wins > randComputer) {
+			losses++;
+			console.log("User Lost");
+			startGame();
+		}
+
+		// check if user has won
+		if (usrScore == randComputer) {
+			wins++;
+			console.log("User Won");
+			startGame();
+		}
 	}
-}
-//end winOrLose
+	//end winOrLose
 
-//Main Game
-$(".crystal").on("click", function(){
-	var clicked = $(this).attr("value");
-	console.log(clicked)
-})
-//End Main Game
+	//Main Game
+	$(".crystal").on("click", function () {
+		var clicked = $(this).attr("value");
+		// console.log(clicked)
+		if (clicked == crystal1) {
+			usrScore += crystalVal1;
+		} else if (clicked == crystal2) {
+			usrScore += crystalVal2;
+		} else if (clicked == crystal3) {
+			usrScore += crystalVal3;
+		} else if (clicked == crystal4) {
+			usrScore += crystalVal4;
+		}
+	})
+	//End Main Game
 
 });
 
