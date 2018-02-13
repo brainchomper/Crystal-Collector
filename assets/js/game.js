f57667daa004c7d6e5ac2be2b707c500783f2899
Kevin Smith <kevin@kvnsmith.com>
2018-02-12 19:33:37 -0700

    minor style update

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,16 +12,16 @@ $(document).ready(function () {
 	function startGame() {
 		// random number between 25 & 80
 
-		randComputer = 25 + Math.floor(Math.random() * 55);
-		crystalVal1 = 1 + Math.floor(Math.random() * 10);
-		crystalVal2 = 1 + Math.floor(Math.random() * 10);;
-		crystalVal3 = 1 + Math.floor(Math.random() * 10);
-		crystalVal4 = 1 + Math.floor(Math.random() * 10);
+		randComputer = 19 + Math.floor(Math.random() * 101);
+		crystalVal1 = 1 + Math.floor(Math.random() * 12);
+		crystalVal2 = 1 + Math.floor(Math.random() * 12);;
+		crystalVal3 = 1 + Math.floor(Math.random() * 12);
+		crystalVal4 = 1 + Math.floor(Math.random() * 12);
 		usrScore = 0;
 		$('#winsCounter').html("Wins: " + wins);
 		$('#lossesCounter').html("Losses: " + losses);
 		$('#randomNumber').html(randComputer);
-		$('#userScore').html("Score: " + usrScore);
+		$('#userScore').html(usrScore);
 		console.log("computer value " + randComputer);
 		console.log("crystal1 val " + crystalVal1);
 		console.log("crystal2 val " + crystalVal2);
@@ -70,18 +70,3 @@ $(document).ready(function () {
 	//End Main Game
 
 });
-
-// When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
-// Your game will hide this amount until the player clicks a crystal.
-// When they do click one, update the player's score counter.
-
-//The player wins if their total score matches the random number from the beginning of the game.
-
-//The player loses if their score goes above the random number.
-
-//The game restarts whenever the player wins or loses.
-
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-//The app should show the number of games the player wins and loses. To that end, do norefresh the page as a means to restart the game
